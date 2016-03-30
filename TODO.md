785fe8bdf2645e5dbd3d15c450d73d547d8e9e5a
yannis <yannis@ianos>
2016-03-30 07:44:56 +0300

    Fixed typeaheads

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
 * Use ui-router
-* Use a proxy (Login issue)
 * Fix fonts (now copied/ uses sass)
-* Typeaheads do not work
 * Create a dev build
--- src/app/child/_module.js
+++ src/app/child/_module.js
@@ -1,22 +1,25 @@
-angular.module('child', ['ngRoute', 'ngResource', 'ui.bootstrap', 'uuid4', 'relationship']).config(['$routeProvider',
+angular.module('child', ['ngRoute', 'ngResource', 'ui.bootstrap', 'uuid4', 'relationship'])
 
-    function ($routeProvider) {
-        $routeProvider
-            .when('/group/:groupId', {
-                templateUrl: 'app/child/listGroup.tpl.html',
-                controller: 'listGroupController'
-            })
-            .when('/child/create', {
-                templateUrl: 'app/child/editChild.tpl.html',
-                controller: 'createChildController'
-            })
-            .when('/child/:childId/update', {
-                templateUrl: 'app/child/editChild.tpl.html',
-                controller: 'updateChildController'
-            })
-            .when('/child/:childId/view', {
-                templateUrl: 'app/child/viewChild.tpl.html',
-                controller: 'updateChildController'
-            });
-    }
-]);
+    .config(['$routeProvider',
+
+        function ($routeProvider) {
+            $routeProvider
+                .when('/group/:groupId', {
+                    templateUrl: 'app/child/listGroup.tpl.html',
+                    controller: 'listGroupController'
+                })
+                .when('/child/create', {
+                    templateUrl: 'app/child/editChild.tpl.html',
+                    controller: 'createChildController'
+                })
+                .when('/child/:childId/update', {
+                    templateUrl: 'app/child/editChild.tpl.html',
+                    controller: 'updateChildController'
+                })
+                .when('/child/:childId/view', {
+                    templateUrl: 'app/child/viewChild.tpl.html',
+                    controller: 'updateChildController'
+                });
+        }
+
+    ]);
