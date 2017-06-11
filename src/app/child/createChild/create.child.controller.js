e4e8b13e88fc2059a0afbc45e208706d6d52ef9e
yannisf <yannisf@ouranos>
2017-06-11 22:16:52 +0300

    value objects

--- src/app/child/createChild/create.child.controller.js
+++ src/app/child/createChild/create.child.controller.js
@@ -1,28 +1,20 @@
-export default function CreateChildCtrl($state, $http, uuid4, TypeaheadSvc, ActiveCache, Child, Address) {
+export default function CreateChildCtrl($state, $http, uuid4, TypeaheadSvc, ActiveCache, Child) {
 
     this.$onInit = function () {
         ActiveCache.child = null;
         this.child = new Child({
-            id: uuid4.generate()
-        });
-        this.address = new Address({
-            id: uuid4.generate()
+            id: uuid4.generate(),
+            address: {}
         });
         this.submitLabel = 'Εισαγωγή';
-
         this.typeaheads = TypeaheadSvc;
     };
 
 
     this.submit = function () {
-        this.address.$save().then(() => {
-            return this.child.$save({
-                addressId: this.address.id,
-                groupId: ActiveCache.group.id
-            });
-        }).then(() => {
-            return $http.get(`api/overview/group/${ActiveCache.group.id}/child`);
-        }).then((response) => {
+        this.child.$save({ groupId: ActiveCache.group.id })
+        .then(() => $http.get(`api/overview/group/${ActiveCache.group.id}/child`))
+        .then((response) => {
             ActiveCache.children = response.data;
             ActiveCache.childIds = response.data.map((child) => child.id);
             ActiveCache.child = this.child;
@@ -39,4 +31,4 @@ export default function CreateChildCtrl($state, $http, uuid4, TypeaheadSvc, Acti
     };
 }
 
-CreateChildCtrl.$inject = ['$state', '$http', 'uuid4', 'TypeaheadSvc', 'ActiveCache', 'Child', 'Address'];
+CreateChildCtrl.$inject = ['$state', '$http', 'uuid4', 'TypeaheadSvc', 'ActiveCache', 'Child'];
